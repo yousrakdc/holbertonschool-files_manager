0a8b7a03261a1b3272e8c8ed6e0a60fc2501480b
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-07-22 15:07:17 +0200

    MongoDB utils

---
+++ utils/db.mjs
@@ -0,0 +1,38 @@
+import { MongoClient } from 'mongodb';
+
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${HOST}:${PORT}`;
+
+class DBClient {
+  constructor() {
+    this.connected = false;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(DATABASE);
+        this.connected = true;
+      })
+      .catch((err) => {
+        console.log(err);
+        this.connected = false;
+      });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
