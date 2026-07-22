c1f9816371ee44142e1ffb6ecea56b58436d9ef4
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-07-22 15:05:40 +0200

    Redis utils

---
+++ utils/redis.mjs
@@ -0,0 +1,41 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.connected = true;
+
+    this.client.on('error', (err) => {
+      console.log(`Redis client not connected to the server: ${err}`);
+      this.connected = false;
+    });
+
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
